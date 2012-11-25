c74205f522ddbe6cdf048e1a13a99c21bb8ab2d1
Stan Lemon <stosh1985@gmail.com>
2012-11-25 17:49:25 -0500

    Added support for checking checkboxes and radio buttons

--- src/Lemon/FormBuilder/Element/Input.php
+++ src/Lemon/FormBuilder/Element/Input.php
@@ -6,4 +6,20 @@ use Lemon\FormBuilder\AbstractElement;
 
 class Input extends AbstractElement implements Element {
 
+	const INPUT_CHECKBOX = 'checkbox';
+	const INPUT_RADIO = 'radio';
+
+	public function setValue($value) {
+		if ($this->dom->getAttribute(self::ATTR_TYPE) == self::INPUT_CHECKBOX || 
+			$this->dom->getAttribute(self::ATTR_TYPE) == self::INPUT_RADIO) {
+			
+			if ($this->dom->getAttribute(self::ATTR_VALUE) == $value) {
+				$this->dom->setAttribute(self::ATTR_CHECKED, self::ATTR_CHECKED);
+			} else {
+				$this->dom->removeAttribute(self::ATTR_CHECKED);
+			}
+		} else {
+			return parent::setValue($value);
+		}
+	}
 }
