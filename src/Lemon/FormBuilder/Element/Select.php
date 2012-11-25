15d36df056d6eef110427290588d644f226d2213
Stan Lemon <stosh1985@gmail.com>
2012-11-25 17:52:16 -0500

    Updated for constants usage

--- src/Lemon/FormBuilder/Element/Select.php
+++ src/Lemon/FormBuilder/Element/Select.php
@@ -7,8 +7,8 @@ class Select extends AbstractElement {
 
 	public function setValue($value) {
 		foreach ($this->dom->childNodes as $child) {
-			if ($child->getAttribute('value') == $value) {
-				$child->setAttribute('selected', 'selected');
+			if ($child->getAttribute(self::ATTR_VALUE) == $value) {
+				$child->setAttribute(self::ATTR_SELECTED, self::ATTR_SELECTED);
 			}
 		}
 	}
